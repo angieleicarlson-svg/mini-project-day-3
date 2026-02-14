fcf42e7e9c5fcafc563bf7a39991f3fa48df6420
angieleicarlson-svg <angieleicarlson@gmail.com>
2026-02-14 10:37:33 -0600

    added refresh upon mouse click

--- project3.js
+++ project3.js
@@ -1,11 +1,13 @@
 function setup() {
     createCanvas(400,400);
-  background(100)
 }
 function draw() {
     
-  //below
   noStroke(0);
   fill(200)
   ellipse(mouseX, mouseY, 10 , 10)
+}
+function mousePressed() {
+   background(100)
+ 
 }
